a022ee5c526009025dafa722a35901e5869a3190
danielp67 <66970422+danielp67@users.noreply.github.com>
2023-07-04 21:07:28 +0200

    CRUD for message

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -2,26 +2,10 @@
 
 namespace App\Controller;
 
-use App\Entity\Message;
-use App\Entity\Room;
-use App\Form\MessageType;
-use App\Form\RoomType;
-use App\Repository\MessageRepository;
 use App\Repository\RoomRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mercure\HubInterface;
-use Symfony\Component\Mercure\Update;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Encoder\XmlEncoder;
-use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
-use Symfony\Component\Serializer\Normalizer\AbstractObjectNormalizer;
-use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
-use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Serializer;
 
 #[Route('/app')]
 class RoomController extends AbstractController
